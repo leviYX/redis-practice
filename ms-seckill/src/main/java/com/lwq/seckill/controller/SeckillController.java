5d5f86805f3b61be7d492fbf78170e89d1d1d536
liuwenqiang <2018305346@qq.com>
2021-08-22 17:35:28 +0800

    秒杀服务搭建，下一步秒杀整合redis

---
+++ ms-seckill/src/main/java/com/lwq/seckill/controller/SeckillController.java
@@ -0,0 +1,51 @@
+package com.lwq.seckill.controller;
+
+import com.lwq.commons.model.domain.ResultInfo;
+import com.lwq.commons.model.pojo.SeckillVouchers;
+import com.lwq.commons.utils.ResultInfoUtil;
+import com.lwq.seckill.service.SeckillService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 秒杀控制层
+ */
+@RestController
+public class SeckillController {
+
+    @Resource
+    private SeckillService seckillService;
+    @Resource
+    private HttpServletRequest request;
+
+    /**
+     * 秒杀下单
+     *
+     * @param voucherId
+     * @param access_token
+     * @return
+     */
+    @PostMapping("{voucherId}")
+    public ResultInfo<String> doSeckill(@PathVariable Integer voucherId, String access_token) {
+        ResultInfo resultInfo = seckillService.doSeckill(voucherId, access_token, request.getServletPath());
+        return resultInfo;
+    }
+
+    /**
+     * 新增秒杀活动，新增这个是是商家在后台添加的，和用户无关，我们项目是给用户做的，所以这个我们就在网关白名单里放行了，不控制了
+     *
+     * @param seckillVouchers
+     * @return
+     */
+    @PostMapping("add")
+    public ResultInfo<String> addSeckillVouchers(@RequestBody SeckillVouchers seckillVouchers) {
+        seckillService.addSeckillVouchers(seckillVouchers);
+        return ResultInfoUtil.buildSuccess(request.getServletPath(),
+                "添加成功");
+    }
+}
